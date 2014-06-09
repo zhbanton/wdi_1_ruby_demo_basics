b2725b913533ebc3c00d4337a4dc9cd4c0660202
zhbanton <zhbanton@gmail.com>
2014-06-09 17:25:54 -0400

    Added code to calculate depreciation

--- vehicle_devaluate.rb
+++ vehicle_devaluate.rb
@@ -1,3 +1,13 @@
 
-
-"___ purchased their first ___ in the year ___ they paided ___ dollars for it and it will be worth ___ dollars in the year 2018"
+print('What\'s your first name?: ')
+first_name = gets.chomp
+print('What\'s your last name?: ')
+last_name = gets.chomp
+make = 'Toyota Highlander'
+year_purchased = 2013
+price = 4000
+value_in_2018 = price
+(2018-year_purchased).times do
+  value_in_2018 *= 0.97
+end
+puts("#{first_name} #{last_name} purchased their first #{make} in the year #{year_purchased} they paid #{price} dollars for it and it will be worth #{value_in_2018} dollars in the year 2018")
